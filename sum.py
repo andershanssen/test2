9fcf3d78fc4b1901fe297a3121e1b6802ab462b2
Anders <andershanssen92@gmail.com>
2016-01-22 10:55:39 +0100

    Added code

--- sum.py
+++ sum.py
@@ -1,1 +1,33 @@
+from threading import Thread, Lock
 
+i = 0
+lock = Lock()
+
+def add():
+    global i
+    for x in range(0,1000000):
+        lock.acquire()
+        i += 1
+        lock.release()
+
+def sub():
+    global i
+    for x in range(0,1000000):
+        lock.acquire()
+        i -= 1
+        lock.release()
+
+def main():
+
+    adder_thr1 = Thread(target = add)
+    adder_thr1.start()
+
+    minus_thr2 = Thread(target = sub)
+    minus_thr2.start()
+
+    adder_thr1.join()
+    minus_thr2.join()
+
+    print("The sum is:", i)
+
+main()
