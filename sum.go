954d85e00675b7ce7a073e79b837a469d14750f2
Anders <andershanssen92@gmail.com>
2016-01-22 10:31:39 +0100

    added code

--- sum.go
+++ sum.go
@@ -1,1 +1,48 @@
 
+package main
+
+import(
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var i = 0
+
+func add(ic chan int){
+	for x := 0; x < 1000000; x++ {
+		ic <- 1
+		i++
+		<-ic
+	}
+}
+
+func sub(ic chan int){
+	for x := 0; x < 1000000; x++ {
+		ic <- 1
+		i--
+		<-ic
+	}
+}
+
+func main(){
+
+	ic := make(chan int, 1)
+
+	GOMAXPROCS(NumCPU())
+	go add(ic)
+	go sub(ic)
+
+	Sleep(1000*Millisecond)
+	Println("The sum is:", i)
+}
+
+
+
+
+
+
+
+
+
+
